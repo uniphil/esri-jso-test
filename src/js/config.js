b8c7fa419bdf0f07effae8763f118db06487848d
phil <uniphil@gmail.com>
2014-07-07 17:57:05 -0400

    rough build config

--- src/js/config.js
+++ src/js/config.js
@@ -21,5 +21,24 @@ var dojoConfig = {
       name: 'app',
       location: 'app'
     }
-  ]
+  ],
+
+  build: {
+    basePath: '.',
+    action: 'release',
+    layers: {
+      'dojo/dojo': [
+        include: ['app'],
+        exclude: [
+          'dojo',
+          'dijit',
+          'dojox',
+          'esri'
+        ],
+        includeLocales: ['en-us'],
+        customBase: true,
+        boot: true
+      ]
+    }
+  }
 }
